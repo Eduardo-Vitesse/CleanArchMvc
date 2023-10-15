f55212f108c5bf1aaa47d53fd42732f06672ef17
Luiz Eduardo F S <edu.dev.py@gmail.com>
2023-10-15 20:25:35 -0300

    Implement the application module

--- CleanArchMvc.Application/Interfaces/ICategoryService.cs
+++ CleanArchMvc.Application/Interfaces/ICategoryService.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CleanArchMvc.Application.DTOs;
 
-namespace CleanArchMvc.Application.Interfaces
+namespace CleanArchMvc.Application.Interfaces;
+
+public interface ICategoryService
 {
-    internal interface ICategoryService
-    {
-    }
+    Task<IEnumerable<CategoryDTO>> GetCategories();
+    Task<CategoryDTO> GetById(int? id);
+    Task Add(CategoryDTO categoryDto);
+    Task Update(CategoryDTO categoryDto);
+    Task Remove(int? id);
 }
